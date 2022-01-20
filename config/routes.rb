db8f59c92a3219a9b0501d27c1ea9b63825d516c
ren <0110ynwa@gmail.com>
2022-01-21 02:12:09 +0900

    user_id と記事の紐付けを実行

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+  registrations: 'users/registrations',
+  sessions: 'users/sessions'
+  }
+
   resources :sumple_articles
   resources :articles
   # トップページの実装
